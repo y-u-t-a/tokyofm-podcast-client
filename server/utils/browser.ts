88a136742ccdc72b48109953d20246bb716a3cc3
Yuta Ueno <y.ueno0803@gmail.com>
2025-10-13 21:55:45 +0900

    Browser の接続が切れていた場合は Browser を close する

--- server/utils/browser.ts
+++ server/utils/browser.ts
@@ -4,12 +4,15 @@ import puppeteer, { Page, Browser } from "puppeteer"
 let _browser: Browser | null = null
 
 async function getBrowser(): Promise<Browser> {
-  if (_browser && _browser.connected) {
-    return _browser
-  } else {
-    _browser = await puppeteer.launch()
-    return _browser
+  if (_browser) {
+    if (_browser.connected) {
+      return _browser
+    } else {
+      _browser.close()
+    }
   }
+  _browser = await puppeteer.launch()
+  return _browser
 }
 
 export async function withBrowser<T>(callback: (page: Page) => Promise<T>): Promise<T> {
